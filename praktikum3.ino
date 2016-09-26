22d5016e4d0820f039e57574126c812e5941fb18
mhanif57 <m.hanief97@gmail.com>
2016-09-26 16:50:44 +0700

    Add files via upload

---
+++ praktikum3.ino
@@ -0,0 +1,18 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(2,3,4,5,6,7);
+
+void setup() {
+lcd.begin(16,2);
+lcd.clear();
+lcd.setCursor(0,0);
+lcd.print("Analog digital pinA0");
+analogReference(EXTERNAL);
+}
+
+void loop() {
+unsigned int dataADC = analogRead(A0);
+lcd.setCursor(0,1);
+lcd.print("nilai = ");
+lcd.print(dataADC);
+delay(100);
+}
